dbda67396375853c73f3b212cfc37c21392d1686
Aaron Brown <kilonow1275@gmail.com>
2017-08-04 11:50:42 -0600

    Prep repo for heroku

--- server.js
+++ server.js
@@ -6,10 +6,22 @@ var express = require('express');
 //Create app by creating variable that calls express library as a function 
 var app = express();
 
+//allows use of host env. const should be all caps by convention
+const PORT = process.env.PORT || 3000;
+
+//request to ensure http traffic(http/https)
+app.use(function(req, res, next) {
+    if(req.headers['x-forwarded-proto']=== 'http') {
+        next();
+    } else {
+        res.redirect('http://'+ req.hostname + req.url);
+    }
+});
+
 //allows for adding functionality
 app.use(express.static('public'));
 
 //start up server with listen function(port, function)
-app.listen(3000, function() {
-    console.log('Express server is up on port 3000')
-})
+app.listen(PORT, function() {
+    console.log('Express server is up on port '+ PORT);
+});
